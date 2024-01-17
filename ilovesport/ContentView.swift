c7aca710bcaabfb7100b554f767d296b3b8e19b5
ViktoriiaKinash <viktoriiakinash@gmail.com>
2024-01-17 22:18:56 +0100

    Initial commit

--- ilovesport/ContentView.swift
+++ ilovesport/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Circle().fill(.cyan).padding().overlay(Image(systemName: "figure.archery"))
         }
         .padding()
     }
