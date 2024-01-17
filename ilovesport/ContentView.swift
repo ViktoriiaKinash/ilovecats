c717bb5f84aee1bdd6ec17e248926ca4b5b40d7a
ViktoriiaKinash <viktoriiakinash@gmail.com>
2024-01-17 23:13:00 +0100

    Display few sports with animation

--- ilovesport/ContentView.swift
+++ ilovesport/ContentView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+
+    @State private var selected = "Archery"
+    @State private var id = 1;
+    
     var body: some View {
-        VStack {
-            Circle().fill(.cyan).padding().overlay(Image(systemName: "figure.archery"))
+        VStack{
+            Text("Why not try...").font(.largeTitle).bold()
+            Spacer()
+            VStack{
+                Circle().fill(colors.randomElement() ?? .cyan).padding().overlay(Image(systemName: "figure.\(selected)".lowercased()).font(.system(size: 144)).foregroundColor(.white))
+                Text("\(selected)!").font(.title)
+            }.transition(.slide).id(id)
+            Spacer()
+            Button("Try again") {
+                withAnimation(.easeInOut(duration: 1)) {
+                    selected = activities.randomElement() ?? "Archery"
+                    id += 1
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
     }
 }
 
