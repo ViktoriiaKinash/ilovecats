9d1516c24b85eb6b27848542c7364f491dd9b768
ViktoriiaKinash <viktoriiakinash@gmail.com>
2024-01-17 22:14:52 +0100

    Initial Commit

---
+++ ilovesport/ilovesportApp.swift
@@ -0,0 +1,17 @@
+//
+//  ilovesportApp.swift
+//  ilovesport
+//
+//  Created by Viktoriia Kinash on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ilovesportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
